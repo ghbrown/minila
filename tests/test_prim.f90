5af8f10296f222726cfd6b65aca31c170737b93f
GHBrown <gabriel.h.brown@gmail.com>
2021-05-26 12:49:13 -0500

    Made matching test for updated strang().

--- tests/test_prim.f90
+++ tests/test_prim.f90
@@ -35,21 +35,28 @@ subroutine test_strang(abs_tol)
 
   implicit none
   real, intent(in) :: abs_tol
+  real, dimension(3,3) :: T_diag
+  real, dimension(3) :: b_diag=(/1.,4.,9./)
   real, dimension(3,3) :: T_upper=transpose(reshape((/1.,2.,3.,0.,4.,5.,0.,0.,6./),(/3,3/)))
   real, dimension(3) :: b_upper=(/14.,23.,18./)
   real, dimension(3,3) :: T_lower=transpose(reshape((/1.,0.,0.,2.,3.,0.,4.,5.,6./),(/3,3/)))
   real, dimension(3) :: b_lower=(/1.,8.,32./)
   real, dimension(3) :: x_ans=(/1.,2.,3./)
-  real, dimension(3) :: x_upper, x_lower
-  logical :: passed_upper, passed_lower
+  real, dimension(3) :: x_diag, x_upper, x_lower
+  logical :: passed_diag, passed_upper, passed_lower
 
+  T_diag=set_diag((/1.,2.,3./)) !initializations must use intrinsic functions
+  !test diagonal solve
+  x_diag=strang(T_diag,b_diag)
+  passed_diag=(norm2(x_diag-x_ans) < abs_tol)
   !test back substitution
   x_upper=strang(T_upper,b_upper)
-  x_lower=strang(T_lower,b_lower)
   passed_upper=(norm2(x_upper-x_ans) < abs_tol)
+  !test forward substitution
+  x_lower=strang(T_lower,b_lower)
   passed_lower=(norm2(x_lower-x_ans) < abs_tol)
 
-  if (passed_upper .and. passed_lower) then
+  if (passed_diag .and. passed_upper .and. passed_lower) then
      print *, "  PASSED,    strang (triangular solve)"
   else
      print *, "  FAILED,    strang (triangular solve)" 
