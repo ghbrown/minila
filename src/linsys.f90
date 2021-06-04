3c90368330061e37559c55b3b54a7577ab6eab86
GHBrown <gabriel.h.brown@gmail.com>
2021-06-04 00:44:30 -0500

    Roughed out linsol()

--- src/linsys.f90
+++ src/linsys.f90
@@ -1,18 +1,45 @@
 
 module linsys
-  !linear system solvers
-  !for square linear system, check for singular A by looking at the diagonla elements of matrix U
-  !if A is singular, one of U's diagonal elements will be 0
+  !Contains routines for the solution of linear systems, and computation of inverse.
+  use prim
+  use decomp
 
-  !check if A is square
-  !call lupp(A,L,U,P)
-  !if (min(abs(get_diag(U))) < sing_tol) then !check diagonal of u to see if A is invertible
-  !print *, "ERROR: Attempted to solve singular linear system"
-  !else
-  !pT(P)=(/(i,i=1,size(p,1))/) !permutation vector corresponding to P^T
-  !b=b(P) !pivot swap b using P vector
-  !y=fwdsub(L,b)
-  !x=bcksub(U,y)
+contains
+
+  function linsol(A,b) return(x)
+    !Solves the linear system Ax=b for x.
+    !---Inputs---
+    !A: full rank square matrix (these are checked)
+    !b: right hand side of linear system
+    !---Outputs---
+    !x: solution to linear system
+    real, intent(in) :: A(:,:)
+    real, intent(in) :: b(size(A,2))
+    real :: x(:)
+    real, dimension(size(A,2),size(A,2)) :: L, U
+    real, dimension(size(A,2)) :: b_permuted, y
+    integer, dimension(size(A,2)) :: p_lhs, p_rhs !left/right hand side row pivot vectors
+    integer :: i, n
+
+    if (.not. is_square(A,n)) then
+       print *, "ERROR: cannot solve nonsquare linear system"
+       return
+    end if
+
+    call lupp(A,L,U,p_lhs)
+
+    if (min(abs(get_diag(U))) == 0.0) then
+       print *, "ERROR: linear system is singular"
+       return
+    end if
+
+  p_rhs(p_lhs)=(/(i,i=1,n)/) !right hand side permutation vector (corresponds to P^T)
+
+  b_permuted=b(p_rhs)
+  y=fwdsub(L,b_permuted) !solve L(Ux)=b_permuted for y=Ux
+  x=bcksub(U,y) !solve Ux=y=(Ux) for x
+
+  end function linsol
 
 
 end module linsys
