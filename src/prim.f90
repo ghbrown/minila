56cc8eb4f09822b44d9b1a8b53f773d7fc7a1414
GHBrown <gabriel.h.brown@gmail.com>
2021-06-04 22:37:00 -0500

    Added more memory-access-optimized version of rop()

--- src/prim.f90
+++ src/prim.f90
@@ -141,16 +141,12 @@ contains
     !---Outputs---
     !u1u2T: order 2 array equal to (u1)transpose(u2)
     implicit none
-    real, intent(in) :: u1(:)
-    real, intent(in) :: u2(:)
-    real :: u1u2T(size(u1),size(u2)) !variable for rank 1 product
-    real :: u1m(size(u1),1) !variable for column matrix of u1
-    real :: u2m(1,size(u2)) !variable for row matrix form of u2
-
-    u1m=reshape(u1,(/size(u1),1/)) !shape first vector into column matrix
-    u2m=reshape(u2,(/1,size(u2)/)) !shape second vector into row matrix
-
-    u1u2T=matmul(u1m,u2m) !compute rank 1 product
+    real, intent(in) :: u1(:), u2(:)
+    real :: u1u2T(size(u1),size(u2))
+    integer :: col
+    do col=1,size(u2)
+        u1u2T(:,col) = u2(col) * u1
+    end do
   end function rop
 
 
